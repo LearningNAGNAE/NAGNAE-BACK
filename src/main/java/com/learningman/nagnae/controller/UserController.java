69aa0ebc89c2e79fa7ed8e0ea6c25d94fb9fa602
yys <lukdown10@gmail.com>
2024-08-03 17:58:32 +0900

    yys

--- src/main/java/com/learningman/nagnae/controller/UserController.java
+++ src/main/java/com/learningman/nagnae/controller/UserController.java
@@ -1,22 +1,17 @@
 package com.learningman.nagnae.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.learningman.nagnae.dto.UserLoginDto;
 import com.learningman.nagnae.dto.UserResponseDto;
 import com.learningman.nagnae.service.UserService;
+import com.learningman.nagnae.util.JsonResult;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
     private final UserService userService;
 
     @Autowired
@@ -25,16 +20,23 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> login(@RequestBody UserLoginDto loginDto) {
-        UserResponseDto response = userService.login(loginDto);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<JsonResult> login(@RequestBody UserLoginDto loginDto) {
+        try {
+            UserResponseDto response = userService.login(loginDto);
+            return ResponseEntity.ok(JsonResult.success(response));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(JsonResult.fail(e.getMessage()));
+        }
     }
     
-
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long id) {
-        UserResponseDto user = userService.getUserById(id);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<JsonResult> getUser(@PathVariable Long id) {
+        try {
+            UserResponseDto user = userService.getUserById(id);
+            return ResponseEntity.ok(JsonResult.success(user));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(JsonResult.fail(e.getMessage()));
+        }
     }
-
 }
